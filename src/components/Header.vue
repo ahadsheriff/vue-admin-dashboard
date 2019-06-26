a1ec2d5fe60389d6ca807fb07d1a9b7808c6baf2
Ahad Sheriff <ahadsheriff@gmail.com>
2019-06-26 00:09:14 -0700

    #3: added sign in flow components, configured styling, and simple page routing

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+    </div>
+</template>
+
+<script>
+    import Header from "@/components/Header.vue"
+    export default {
+        name: "home",
+        components: {
+            HelloWorld,
+            Header
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
