e4b9ef7db3ce70d7dc9d1b2f29060855c68be329
Ahad Sheriff <ahadsheriff@gmail.com>
2019-06-29 16:46:43 -0700

    addeded all transitions and animations

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,15 @@
 <template>
-    <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-    </div>
+  <div id="nav">
+    <router-link to="/">Home</router-link>|
+    <router-link to="/about">About</router-link>
+  </div>
 </template>
 
 <script>
-    import Header from "@/components/Header.vue"
-    export default {
-        name: "home",
-        components: {
-            HelloWorld,
-            Header
-        }
-    }
+export default {
+  name: "Header"
+};
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="hello">
-    {{ msg }}
-  </div>
+  <div class="hello">{{ msg }}</div>
 </template>
 
 <script>
@@ -15,18 +13,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
